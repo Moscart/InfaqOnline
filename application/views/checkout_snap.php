581664057f0a5f5694a8d122e33f5f4f236859e4
volumeee <mr.volumee@gmail.com>
2022-08-11 15:25:17 +0700

    http://localhost/infaqonline/index.php/snap

--- application/views/checkout_snap.php
+++ application/views/checkout_snap.php
@@ -10,56 +10,62 @@
 
 
   <form id="payment-form" method="post" action="<?= site_url() ?>/snap/finish">
-    <input type="hidden" name="result_type" id="result-type" value=""></div>
-    <input type="hidden" name="result_data" id="result-data" value=""></div>
+    <input type="number" min="10000" name="result_type" id="result-type" value=""></div>
+    <!-- <input type="text" name="result_data" id="result-data" value=""></div> -->
   </form>
 
   <button id="pay-button">Pay!</button>
   <script type="text/javascript">
     $('#pay-button').click(function(event) {
       event.preventDefault();
-      $(this).attr("disabled", "disabled");
+      let nominal = Number($('#result-type').val());
+      if (nominal >= 10000) {
+        $(this).attr("disabled", "disabled");
 
-      $.ajax({
-        url: '<?= site_url() ?>/snap/token',
-        cache: false,
+        $.ajax({
+          url: '<?= site_url() ?>/snap/token/' + nominal,
+          cache: false,
 
-        success: function(data) {
-          //location = data;
+          success: function(data) {
+            //location = data;
 
-          console.log('token = ' + data);
+            console.log('token = ' + data);
 
-          var resultType = document.getElementById('result-type');
-          var resultData = document.getElementById('result-data');
+            var resultType = document.getElementById('result-type');
+            var resultData = document.getElementById('result-data');
 
-          function changeResult(type, data) {
-            $("#result-type").val(type);
-            $("#result-data").val(JSON.stringify(data));
-            //resultType.innerHTML = type;
-            //resultData.innerHTML = JSON.stringify(data);
-          }
-
-          snap.pay(data, {
-
-            onSuccess: function(result) {
-              changeResult('success', result);
-              console.log(result.status_message);
-              console.log(result);
-              $("#payment-form").submit();
-            },
-            onPending: function(result) {
-              changeResult('pending', result);
-              console.log(result.status_message);
-              $("#payment-form").submit();
-            },
-            onError: function(result) {
-              changeResult('error', result);
-              console.log(result.status_message);
-              $("#payment-form").submit();
+            function changeResult(type, data) {
+              $("#result-type").val(type);
+              $("#result-data").val(JSON.stringify(data));
+              //resultType.innerHTML = type;
+              //resultData.innerHTML = JSON.stringify(data);
             }
-          });
-        }
-      });
+
+            snap.pay(data, {
+
+              onSuccess: function(result) {
+                changeResult('success', result);
+                console.log(result.status_message);
+                console.log(result);
+                $("#payment-form").submit();
+              },
+              onPending: function(result) {
+                changeResult('pending', result);
+                console.log(result.status_message);
+                $("#payment-form").submit();
+              },
+              onError: function(result) {
+                changeResult('error', result);
+                console.log(result.status_message);
+                $("#payment-form").submit();
+              }
+            });
+          }
+        });
+      } else {
+        alert('Nominal minimal Rp. 10.000');
+      }
+
     });
   </script>
 
