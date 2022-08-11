19cb5a7713f97baed0dbe0fb7bd1a7a05c6038de
yfbahtiar <yusuf2bahtiar@gmail.com>
2022-08-11 12:23:41 +0700

    rubah base_url config.php agar dinamis

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -93,7 +93,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 		<div id="body">
 			<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
-			<p>If you wouldssss like to edit this page you'll find it located at:</p>
+			<p>If you would like to edit this page you'll find it located at:</p>
 			<code>application/views/welcome_message.php</code>
 
 			<p>The corresponding controller for this page is found at:</p>
